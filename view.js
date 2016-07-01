41911a601c652d6edaffd99138fde3fc8a8f53d3
George Francis <georgefrancis105@gmail.com>
2016-07-01 16:53:26 +1200

    add deploy modules

--- view.js
+++ view.js
@@ -5,10 +5,11 @@ export default class View {
     console.log("creating ufo", model.name)
     $('#draw-window').append('<div class="ufo" id="' + model.id + '"></div>')
     console.log("made thing: #" + model.id)
+    $('#' + model.id).css({width: model.size, height: model.size, background: model.colour, position:'absolute'})
   }
   render (model) {
-    let xPos = model.getXPosition()
-    let yPos = model.getYPosition()
+    let xPos = model.getXPosition() - (model.size / 2)
+    let yPos = model.getYPosition() - (model.size / 2)
     // console.log("rendering ufo:", model.name)
     // console.log("        X pos:", model.getXPosition())
     // console.log("        Y pos:", model.getYPosition())
