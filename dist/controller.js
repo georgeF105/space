51682f602032972d3b63dc64126c18adb0400f1d
George Francis <georgefrancis105@gmail.com>
2016-07-02 15:00:05 +1200

    Deploy project

--- dist/controller.js
+++ dist/controller.js
@@ -1,28 +1,44 @@
 import Ufo from './models/ufo'
 import View from './view'
+import $ from 'jquery'
+
 
 export default class Controller {
   constructor() {
     console.log("construct things")
-    this.sun = new Ufo({x:250,y:250,xV:0,yV:0}, 100, 50, '#f78303', 'the_sun')
-    this.planet = new Ufo({x:250,y:100,xV:35,yV:0 }, 100, 10, 'green', 'the_earth')
-    this.planet2 = new Ufo({x:250,y:80,xV:35,yV:0 }, 100, 10, 'green', 'the_earth2')
     this.view = new View()
-    this.view.create(this.sun)
-    this.view.create(this.planet)
-    this.view.create(this.planet2)
-    console.log('name',this.sun)
-    // this.sun.refreshPosition()
-    console.log('here')
+    const v = this.view
+    this.ufos = []
+    // this.ufos.push(new Ufo({x:250,y:250,xV:0,yV:0}, 100, 50, '#f78303', 'the_sun'))
+    let halfWidth = $('#draw-window').width()/2
+    let halfHeight = $('#draw-window').height()/2
+    console.log('halfHeight',halfHeight)
+    this.ufos.push(new Ufo({x:halfWidth - 200,y:halfHeight - 200,xV:35,yV:0 }, 100, 15, '#00effe', 'the_earth'))
+    this.ufos.push(new Ufo({x:halfWidth + 200,y:halfHeight - 200,xV:0,yV:35 }, 100, 15, '#00effe', 'the_earth2'))
+    this.ufos.push(new Ufo({x:halfWidth + 200,y:halfHeight + 200,xV:-35,yV:0 }, 100, 15, '#00effe', 'the_earth3'))
+    this.ufos.push(new Ufo({x:halfWidth - 200,y:halfHeight + 200,xV:0,yV:-35 }, 100, 15, '#00effe', 'the_earth4'))
+
+    this.ufos.forEach(function(ufo){
+      v.create(ufo)
+    })
   }
   // this.sun.refreshPosition()
   update() {
     // console.log('this.sun',this.sun)
     // this.sun.refreshPosition()
-    this.planet.refreshPosition(this.sun)
-    this.planet2.refreshPosition(this.sun)
-    this.view.render(this.sun)
-    this.view.render(this.planet)
-    this.view.render(this.planet2)
+    const v = this.view
+    const ufos = this.ufos
+    for(let i = 0; i < this.ufos.length; i++){
+    // this.ufos.forEach(function(ufo, index){
+      let ufo = this.ufos[i]
+      ufo.refreshPosition(ufos)
+      v.render(ufo)
+    }
+    // })
+    // this.planet.refreshPosition(this.sun)
+    // this.planet2.refreshPosition(this.sun)
+    // this.view.render(this.sun)
+    // this.view.render(this.planet)
+    // this.view.render(this.planet2)
   }
 }
--- dist/view.js
+++ dist/view.js
@@ -5,7 +5,9 @@ export default class View {
     console.log("creating ufo", model.name)
     $('#draw-window').append('<div class="ufo" id="' + model.id + '"></div>')
     console.log("made thing: #" + model.id)
-    $('#' + model.id).css({width: model.size, height: model.size, background: model.colour, position:'absolute'})
+    let xPos = model.getXPosition() - (model.size / 2)
+    let yPos = model.getYPosition() - (model.size / 2)
+    $('#' + model.id).css({width: model.size, height: model.size, top:yPos, left:xPos, background: model.colour, position:'absolute'})
   }
   render (model) {
     let xPos = model.getXPosition() - (model.size / 2)
@@ -13,6 +15,11 @@ export default class View {
     // console.log("rendering ufo:", model.name)
     // console.log("        X pos:", model.getXPosition())
     // console.log("        Y pos:", model.getYPosition())
-    $('#' + model.id).css({top: yPos, left: xPos})
+    if(xPos > $('#draw-window').width() || xPos < 0 || yPos > $('#draw-window').height() || yPos < 0){
+      $('#' + model.id).css({display: 'none'})
+    }
+    else{
+      $('#' + model.id).css({display: 'inital',top: yPos, left: xPos})
+    }
   }
 }
